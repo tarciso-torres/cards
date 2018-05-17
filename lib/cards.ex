1fd4d755fd12487aa0e946250accfb9cedf6aa2b
Tarciso Torres <tarcisoftorres@gmail.com>
2018-05-16 22:01:17 -0300

    Start List

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three", "Four"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonts"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   def shuffle(deck) do 
