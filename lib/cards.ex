a593a4db560dd21a8ef66421a369d89209268877
Tarciso Torres <tarciso.torres@ifactory.com.br>
2018-05-18 13:18:45 -0300

    Add method save in cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -19,5 +19,10 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
   
 end
